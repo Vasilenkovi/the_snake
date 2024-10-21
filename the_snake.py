e30f4e00dc523e8bfca6fd5823ed262362250d34
ShizVizor <vladimir.vasilenko.0303@mail.ru>
2024-10-21 09:34:16 +0700

    Commit with basic version

--- the_snake.py
+++ the_snake.py
@@ -40,25 +40,133 @@ clock = pygame.time.Clock()
 
 
 # Тут опишите все классы игры.
-...
+
+class GameObject:
+    def __init__(self, body_color, position):
+        self.position = position
+        self.body_color = body_color
+
+    def draw(self):
+        pass
+
+
+class Apple(GameObject):
+    def __init__(self):
+        super().__init__(APPLE_COLOR, self.randomize_position())
+
+    @classmethod
+    def randomize_position(cls):
+        return randint(0, 64) * 10, randint(0, 38) * 10
+
+    def draw(self):
+        print()
+        rect = pygame.Rect(self.position, (GRID_SIZE, GRID_SIZE))
+        pygame.draw.rect(screen, self.body_color, rect)
+        pygame.draw.rect(screen, BORDER_COLOR, rect, 1)
+
+
+class Snake(GameObject):
+    def __init__(self):
+        super().__init__(SNAKE_COLOR, (320, 240))
+        self.positions = [self.position]
+        self.length = 1
+        self.direction = RIGHT
+        self.next_direction = None
+        self.last = None
+
+    def update_direction(self):
+        if self.next_direction:
+            self.direction = self.next_direction
+            self.next_direction = None
+
+    def move(self, apple_not_eaten=True):
+        next_position1 = (self.direction[0] * 10 + self.get_head_position()[0]) % 640
+        next_position2 = (self.direction[1] * 10 + self.get_head_position()[1]) % 480
+        self.positions.insert(0, (next_position1, next_position2))
+        self.last = self.positions[-1]
+        self.length += 1
+        if apple_not_eaten:
+            self.length -= 1
+            self.positions = self.positions[0: -1]
+
+
+    def get_head_position(self):
+        return self.positions[0]
+
+    def draw(self):
+        for position in self.positions[:-1]:
+            rect = (pygame.Rect(position, (GRID_SIZE, GRID_SIZE)))
+            pygame.draw.rect(screen, self.body_color, rect)
+            pygame.draw.rect(screen, BORDER_COLOR, rect, 1)
+
+        # Отрисовка головы змейки
+        head_rect = pygame.Rect(self.positions[0], (GRID_SIZE, GRID_SIZE))
+        pygame.draw.rect(screen, self.body_color, head_rect)
+        pygame.draw.rect(screen, BORDER_COLOR, head_rect, 1)
+
+        # Затирание последнего сегмента
+        if self.last:
+            last_rect = pygame.Rect(self.last, (GRID_SIZE, GRID_SIZE))
+            pygame.draw.rect(screen, BOARD_BACKGROUND_COLOR, last_rect)
+
+    def reset(self):
+        if self.length > 1:
+            for pos in self.positions[1:]:
+                x_apple = pos[0] == self.get_head_position()[0]
+                y_apple = pos[1] == self.get_head_position()[1]
+                if x_apple and y_apple:
+                    self.__init__()
+                    self.draw()
+                    break
+
+
+def handle_keys(game_object):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            raise SystemExit
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP and game_object.direction != DOWN:
+                game_object.next_direction = UP
+            elif event.key == pygame.K_DOWN and game_object.direction != UP:
+                game_object.next_direction = DOWN
+            elif event.key == pygame.K_LEFT and game_object.direction != RIGHT:
+                game_object.next_direction = LEFT
+            elif event.key == pygame.K_RIGHT and game_object.direction != LEFT:
+                game_object.next_direction = RIGHT
 
 
 def main():
     # Инициализация PyGame:
     pygame.init()
     # Тут нужно создать экземпляры классов.
-    ...
-
-    # while True:
-    #     clock.tick(SPEED)
-
-        # Тут опишите основную логику игры.
-        # ...
+    snake = Snake()
+    apple = Apple()
+    snake.draw()
+    apple.draw()
+    while True:
+        clock.tick(SPEED)
+        handle_keys(snake)
+        snake.update_direction()
+        x_apple = apple.position[0] - snake.get_head_position()[0]
+        y_apple = apple.position[1] - snake.get_head_position()[1]
+        print(x_apple, y_apple)
+        if abs(x_apple) + abs(y_apple) < 15:
+            print("EATEN++++++++++++")
+            snake.move(False)
+            apple.body_color = (0,0,0)
+            apple.draw()
+            apple = Apple()
+            apple.draw()
+        else:
+            snake.move()
+        snake.reset()
+        snake.draw()
+        pygame.display.update()
 
 
 if __name__ == '__main__':
     main()
-
 
 # Метод draw класса Apple
 # def draw(self):
